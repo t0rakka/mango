434e573b1ee47e184ec1c3ae718f84a29872d185
Jukka Liimatta <jukka@liimatta.org>
2017-01-02 22:55:48 +0200

    Rename variables to document their convention better.

--- include/mango/core/bits.hpp
+++ include/mango/core/bits.hpp
@@ -38,7 +38,7 @@ namespace mango
 
 #elif defined(MANGO_COMPILER_GCC) || defined(MANGO_COMPILER_CLANG)
 
-    // GCC / LLVM intrinsics
+    // GCC / CLANG intrinsics
 
     static inline uint16 byteswap16(uint16 v)
     {
@@ -57,7 +57,10 @@ namespace mango
 
 #else
 
-    // generic implementation
+    // Generic implementation
+
+    // These idioms are often recognized by compilers and result in bswap instruction being generated
+    // but cannot be guaranteed so compiler intrinsics above are preferred when available.
 
     static inline uint16 byteswap16(uint16 v)
     {
@@ -71,6 +74,8 @@ namespace mango
 
     static inline uint64 byteswap64(uint64 v)
     {
+        // We could unroll the computation to be done with 64 bits but this approach results in
+        // simpler and more efficient code (has been measured on gcc 5.4 on x86-64 and ARMv7)
         uint32 low = v & 0xffffffff;
         uint32 high = v >> 32;
         low = byteswap32(low);
@@ -79,6 +84,8 @@ namespace mango
     }
 
 #endif
+
+    // byteswap templates
 
     template <typename Type>
     static inline Type byteswap(Type v)
@@ -231,103 +238,102 @@ namespace mango
     // 32 bits
     // ----------------------------------------------------------------------------
 
-    static inline uint32 u32_expand_lsb(uint32 v)
+    static inline uint32 u32_expand_lsb(uint32 value)
     {
 		// NOTE: 0 expands to 0xffffffff
-        return v ^ (v - 1);
+        return value ^ (value - 1);
     }
 
-    static inline uint32 u32_expand_msb(uint32 v)
+    static inline uint32 u32_expand_msb(uint32 value)
     {
-        v |= v >> 1;
-        v |= v >> 2;
-        v |= v >> 4;
-        v |= v >> 8;
-        v |= v >> 16;
-        return v;
+        value |= value >> 1;
+        value |= value >> 2;
+        value |= value >> 4;
+        value |= value >> 8;
+        value |= value >> 16;
+        return value;
     }
 
-    static inline uint32 u32_lsb(uint32 v)
+    static inline uint32 u32_lsb(uint32 value)
     {
-        return v & (0 - v);
+        return value & (0 - value);
     }
 
-    static inline uint32 u32_msb(uint32 v)
+    static inline uint32 u32_msb(uint32 value)
     {
-        v = u32_expand_msb(v);
-        return (v + 1) >> 1;
+        const uint32 mask = u32_expand_msb(value);
+        return (mask + 1) >> 1;
     }
 
-    static inline int u32_index_of_bit(uint32 v)
+    static inline int u32_index_of_bit(uint32 bit)
     {
         static const uint8 table[] =
         {
             0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
             31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
         };
-        return table[(v * 0x077cb531) >> 27];
+        return table[(bit * 0x077cb531) >> 27];
     }
 
-    static inline int u32_index_of_expanded_bit(uint32 v)
+    static inline int u32_index_of_expanded_bit(uint32 mask)
     {
         static const uint8 table[] =
         {
 			0, 9, 1, 10, 13, 21, 2, 29, 11, 14, 16, 18, 22, 25, 3, 30,
 			8, 12, 20, 28, 15, 17, 24, 7, 19, 27, 23, 6, 26, 5, 4, 31
         };
-        return table[(v * 0x07c4acdd) >> 27];
+        return table[(mask * 0x07c4acdd) >> 27];
     }
 
-    static inline int u32_index_of_lsb(uint32 v)
+    static inline int u32_index_of_lsb(uint32 value)
     {
-        v = u32_lsb(v);
-        return u32_index_of_bit(v);
+        const uint32 lsb = u32_lsb(value);
+        return u32_index_of_bit(lsb);
     }
 
-    static inline int u32_index_of_msb(uint32 v)
+    static inline int u32_index_of_msb(uint32 value)
     {
         int base = 0;
-        uint32 n;
-        n = v & 0xffff0000; if (n) { base |= 16; v = n; }
-        n = v & 0xff00ff00; if (n) { base |= 8;  v = n; }
-        n = v & 0xf0f0f0f0; if (n) { base |= 4;  v = n; }
-        n = v & 0xcccccccc; if (n) { base |= 2;  v = n; }
-        n = v & 0xaaaaaaaa; if (n) { base |= 1; }
+        uint32 temp;
+        temp = value & 0xffff0000; if (temp) { base |= 16; value = temp; }
+        temp = value & 0xff00ff00; if (temp) { base |= 8;  value = temp; }
+        temp = value & 0xf0f0f0f0; if (temp) { base |= 4;  value = temp; }
+        temp = value & 0xcccccccc; if (temp) { base |= 2;  value = temp; }
+        temp = value & 0xaaaaaaaa; if (temp) { base |= 1; }
         return base;
     }
 
-    static inline int u32_index_of_msb_in_mask(uint32 v)
+    static inline int u32_index_of_msb_in_mask(uint32 mask)
     {
-        v = v & ~(v >> 1);
-        return u32_index_of_bit(v);
+        const uint32 msb = mask & ~(mask >> 1);
+        return u32_index_of_bit(msb);
     }
 
-    static inline uint32 u32_reverse_bits(uint32 v)
+    static inline uint32 u32_reverse_bits(uint32 value)
     {
-        // reverse bits
-        v = ((v >> 1) & 0x55555555) | ((v << 1) & 0xaaaaaaaa);
-        v = ((v >> 2) & 0x33333333) | ((v << 2) & 0xcccccccc);
-        v = ((v >> 4) & 0x0f0f0f0f) | ((v << 4) & 0xf0f0f0f0);
-        v = ((v >> 8) & 0x00ff00ff) | ((v << 8) & 0xff00ff00);
-        v = (v >> 16) | (v << 16);
-        return v;
+        value = ((value >> 1) & 0x55555555) | ((value << 1) & 0xaaaaaaaa);
+        value = ((value >> 2) & 0x33333333) | ((value << 2) & 0xcccccccc);
+        value = ((value >> 4) & 0x0f0f0f0f) | ((value << 4) & 0xf0f0f0f0);
+        value = ((value >> 8) & 0x00ff00ff) | ((value << 8) & 0xff00ff00);
+        value = (value >> 16) | (value << 16);
+        return value;
     }
 
 #if defined(MANGO_ENABLE_POPCNT)
 
-    static inline int u32_count_bits(uint32 v)
+    static inline int u32_count_bits(uint32 value)
     {
-		return _mm_popcnt_u32(v);
+		return _mm_popcnt_u32(value);
     }
 
 #else
 
-    static inline int u32_count_bits(uint32 v)
+    static inline int u32_count_bits(uint32 value)
     {
-        v -= (v >> 1) & 0x55555555;
-        v = (v & 0x33333333) + ((v >> 2) & 0x33333333);
-        v = (v + (v >> 4)) & 0x0f0f0f0f;
-        return int((v * 0x01010101) >> 24);
+        value -= (value >> 1) & 0x55555555;
+        value = (value & 0x33333333) + ((value >> 2) & 0x33333333);
+        value = (value + (value >> 4)) & 0x0f0f0f0f;
+        return int((value * 0x01010101) >> 24);
     }
 
 #endif
@@ -344,22 +350,22 @@ namespace mango
         return (value & (value - 1)) == 0;
     }
 
-    static inline uint32 u32_floor_power_of_two(uint32 v)
+    static inline uint32 u32_floor_power_of_two(uint32 value)
     {
-        v = u32_expand_msb(v);
-        return (v + 1) >> 1;
+        const uint32 mask = u32_expand_msb(value);
+        return (mask + 1) >> 1;
     }
 
-    static inline uint32 u32_ceil_power_of_two(uint32 v)
+    static inline uint32 u32_ceil_power_of_two(uint32 value)
     {
-        v = u32_expand_msb(v - 1);
-        return v + 1;
+        const uint32 mask = u32_expand_msb(value - 1);
+        return mask + 1;
     }
 
-    static inline int u32_log2(uint32 v)
+    static inline int u32_log2(uint32 value)
     {
-        v = u32_expand_msb(v);
-        return u32_index_of_expanded_bit(v);
+        const uint32 mask = u32_expand_msb(value);
+        return u32_index_of_expanded_bit(mask);
     }
 
 #ifdef MANGO_ENABLE_BMI
@@ -380,9 +386,9 @@ namespace mango
 
 #ifdef MANGO_ENABLE_BMI2
 
-    static inline uint32 u32_interleave_bits(uint32 v)
+    static inline uint32 u32_interleave_bits(uint32 value)
     {
-        return _pdep_u32(v, 0x55555555);
+        return _pdep_u32(value, 0x55555555);
     }
 
     static inline uint32 u32_encode_morton(uint32 x, uint32 y)
@@ -392,13 +398,13 @@ namespace mango
 
 #else
 
-    static inline uint32 u32_interleave_bits(uint32 v)
+    static inline uint32 u32_interleave_bits(uint32 value)
     {
-        v = (v | (v << 8)) & 0x00ff00ff;
-        v = (v | (v << 4)) & 0x0f0f0f0f;
-        v = (v | (v << 2)) & 0x33333333;
-        v = (v | (v << 1)) & 0x55555555;
-        return v;
+        value = (value | (value << 8)) & 0x00ff00ff;
+        value = (value | (value << 4)) & 0x0f0f0f0f;
+        value = (value | (value << 2)) & 0x33333333;
+        value = (value | (value << 1)) & 0x55555555;
+        return value;
     }
 
     static inline uint32 u32_encode_morton(uint32 x, uint32 y)
@@ -410,46 +416,46 @@ namespace mango
 
 #endif
 
-    static inline bool u32_has_zero_byte(uint32 v)
+    static inline bool u32_has_zero_byte(uint32 value)
     {
         const uint32 mask = 0x7f7f7f7f;
-        v = ((v & mask) + mask) | v;
-        return ~(v | mask) != 0;
+        value = ((value & mask) + mask) | value;
+        return ~(value | mask) != 0;
     }
 
     // ----------------------------------------------------------------------------
     // 64 bits
     // ----------------------------------------------------------------------------
 
-    static inline uint64 u64_expand_lsb(uint64 v)
+    static inline uint64 u64_expand_lsb(uint64 value)
     {
 		// NOTE: 0 expands to 0xffffffffffffffff
-        return v ^ (v - 1);
+        return value ^ (value - 1);
     }
 
-    static inline uint64 u64_expand_msb(uint64 v)
+    static inline uint64 u64_expand_msb(uint64 value)
     {
-        v |= v >> 1;
-        v |= v >> 2;
-        v |= v >> 4;
-        v |= v >> 8;
-        v |= v >> 16;
-        v |= v >> 32;
-        return v;
+        value |= value >> 1;
+        value |= value >> 2;
+        value |= value >> 4;
+        value |= value >> 8;
+        value |= value >> 16;
+        value |= value >> 32;
+        return value;
     }
 
-    static inline uint64 u64_lsb(uint64 v)
+    static inline uint64 u64_lsb(uint64 value)
     {
-        return v & (0 - v);
+        return value & (0 - value);
     }
 
-    static inline uint64 u64_msb(uint64 v)
+    static inline uint64 u64_msb(uint64 value)
     {
-        v = u64_expand_msb(v);
-        return (v + 1) >> 1;
+        const uint64 mask = u64_expand_msb(value);
+        return (mask + 1) >> 1;
     }
 
-    static inline int u64_index_of_bit(uint64 v)
+    static inline int u64_index_of_bit(uint64 bit)
     {
         static const uint8 table[] =
         {
@@ -462,10 +468,10 @@ namespace mango
             51, 25, 36, 32, 60, 20, 57, 16,
             50, 31, 19, 15, 30, 14, 13, 12,
         };
-        return table[(v * 0x022fdd63cc95386du) >> 58];
+        return table[(bit * 0x022fdd63cc95386du) >> 58];
     }
 
-    static inline int u64_index_of_expanded_bit(uint64 v)
+    static inline int u64_index_of_expanded_bit(uint64 mask)
     {
         static const uint8 table[] =
         {
@@ -478,44 +484,44 @@ namespace mango
             25, 39, 14, 33, 19, 30, 9, 24,
             13, 18, 8, 12, 7, 6, 5, 63
         };
-        return table[(v * 0x03f79d71b4cb0a89u) >> 58];
+        return table[(mask * 0x03f79d71b4cb0a89u) >> 58];
     }
 
-    static inline int u64_index_of_lsb(uint64 v)
+    static inline int u64_index_of_lsb(uint64 value)
     {
-        v = u64_lsb(v);
-        return u64_index_of_bit(v);
+        const uint64 lsb = u64_lsb(value);
+        return u64_index_of_bit(lsb);
     }
 
-    static inline int u64_index_of_msb(uint64 v)
+    static inline int u64_index_of_msb(uint64 value)
     {
-        v = u64_expand_msb(v);
-        return u64_index_of_expanded_bit(v);
+        const uint64 msb = u64_expand_msb(value);
+        return u64_index_of_expanded_bit(msb);
     }
 
 #if defined(MANGO_ENABLE_POPCNT)
 
-    static inline int u64_count_bits(uint64 v)
+    static inline int u64_count_bits(uint64 value)
     {
     #if defined(MANGO_CPU_64BIT)
-        return static_cast<int>(_mm_popcnt_u64(v));
+        return static_cast<int>(_mm_popcnt_u64(value));
     #else
         // popcnt_u64 is invalid instruction in 32 bit legacy mode
-        uint32 low = v & 0xffffffff;
-        uint32 high = v >> 32;
+        uint32 low = value & 0xffffffff;
+        uint32 high = value >> 32;
         return _mm_popcnt_u32(low) + _mm_popcnt_u32(high);
     #endif
     }
 
 #else
 
-    static inline int u64_count_bits(uint64 v)
+    static inline int u64_count_bits(uint64 value)
     {
         const uint64 c = 0x3333333333333333u;
-        v -= (v >> 1) & 0x5555555555555555u;
-        v = (v & c) + ((v >> 2) & c);
-        v = (v + (v >> 4)) & 0x0f0f0f0f0f0f0f0fu;
-        return int((v * 0x0101010101010101u) >> 56);
+        value -= (value >> 1) & 0x5555555555555555u;
+        value = (value & c) + ((value >> 2) & c);
+        value = (value + (value >> 4)) & 0x0f0f0f0f0f0f0f0fu;
+        return int((value * 0x0101010101010101u) >> 56);
     }
 
 #endif
@@ -538,9 +544,9 @@ namespace mango
 
 #ifdef MANGO_ENABLE_BMI2
 
-    static inline uint64 u64_interleave_bits(uint64 v)
+    static inline uint64 u64_interleave_bits(uint64 value)
     {
-        return _pdep_u64(x, 0x55555555);
+        return _pdep_u64(value, 0x55555555);
     }
 
     static inline uint64 u64_encode_morton(uint64 x, uint64 y)
@@ -550,14 +556,14 @@ namespace mango
 
 #else
 
-    static inline uint64 u64_interleave_bits(uint64 v)
+    static inline uint64 u64_interleave_bits(uint64 value)
     {
-        v = (v | (v << 16)) & 0x0000ffff0000ffff;
-        v = (v | (v <<  8)) & 0x00ff00ff00ff00ff;
-        v = (v | (v <<  4)) & 0x0f0f0f0f0f0f0f0f;
-        v = (v | (v <<  2)) & 0x3333333333333333;
-        v = (v | (v <<  1)) & 0x5555555555555555;
-        return v;
+        value = (value | (value << 16)) & 0x0000ffff0000ffff;
+        value = (value | (value <<  8)) & 0x00ff00ff00ff00ff;
+        value = (value | (value <<  4)) & 0x0f0f0f0f0f0f0f0f;
+        value = (value | (value <<  2)) & 0x3333333333333333;
+        value = (value | (value <<  1)) & 0x5555555555555555;
+        return value;
     }
 
     static inline uint64 u64_encode_morton(uint64 x, uint64 y)
@@ -569,11 +575,11 @@ namespace mango
 
 #endif
 
-    static inline bool u64_has_zero_byte(uint64 v)
+    static inline bool u64_has_zero_byte(uint64 value)
     {
         const uint64 mask = 0x7f7f7f7f7f7f7f7f;
-        v = ((v & mask) + mask) | v;
-        return ~(v | mask) != 0;
+        value = ((value & mask) + mask) | value;
+        return ~(value | mask) != 0;
     }
 
     static inline bool u64_is_power_of_two(uint64 value)
