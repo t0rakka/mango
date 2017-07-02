4e6f7bae5241f2f68e9f65a885aa5e9b3e912787
Jukka Liimatta <jukka@liimatta.org>
2017-07-02 18:22:00 +0300

    Add masked simd gather.

--- include/mango/simd/gather.hpp
+++ include/mango/simd/gather.hpp
@@ -79,6 +79,70 @@ namespace simd {
         return _mm256_i32gather_epi64(reinterpret_cast<const long long*>(address), offset, 8);
     }
 
+    // 128 bit masked gather
+
+    static inline float32x4 gather4(const float* address, int32x4 offset, float32x4 value, float32x4 mask)
+    {
+        return _mm_mask_i32gather_ps(value, reinterpret_cast<const float*>(address), offset, mask, 4);
+    }
+
+    static inline float64x2 gather2(const double* address, int32x4 offset, float64x2 value, float64x2 mask)
+    {
+        return _mm_mask_i32gather_pd(value, reinterpret_cast<const double*>(address), offset, mask, 8);
+    }
+
+    static inline uint32x4 gather4(const uint32* address, int32x4 offset, uint32x4 value, uint32x4 mask)
+    {
+        return _mm_mask_i32gather_epi32(value, reinterpret_cast<const int*>(address), offset, mask, 4);
+    }
+
+    static inline int32x4 gather4(const int32* address, int32x4 offset, int32x4 value, int32x4 mask)
+    {
+        return _mm_mask_i32gather_epi32(value, reinterpret_cast<const int*>(address), offset, mask, 4);
+    }
+
+    static inline uint64x2 gather2(const uint64* address, int32x4 offset, uint64x2 value, uint64x2 mask)
+    {
+        return _mm_mask_i32gather_epi64(value, reinterpret_cast<const long long*>(address), offset, mask, 8);
+    }
+
+    static inline int64x2 gather2(const int64* address, int32x4 offset, int64x2 value, int64x2 mask)
+    {
+        return _mm_mask_i32gather_epi64(value, reinterpret_cast<const long long*>(address), offset, mask, 8);
+    }
+
+    // 256 bit masked gather
+
+    static inline float32x8 gather8(const float* address, int32x8 offset, float32x8 value, float32x8 mask)
+    {
+        return _mm256_mask_i32gather_ps(value, reinterpret_cast<const float*>(address), offset, mask, 4);
+    }
+
+    static inline float64x4 gather4(const double* address, int32x4 offset, float64x4 value, float64x4 mask)
+    {
+        return _mm256_mask_i32gather_pd(value, reinterpret_cast<const double*>(address), offset, mask, 8);
+    }
+
+    static inline uint32x8 gather8(const uint32* address, int32x8 offset, uint32x8 value, uint32x8 mask)
+    {
+        return _mm256_mask_i32gather_epi32(value, reinterpret_cast<const int*>(address), offset, mask, 4);
+    }
+
+    static inline int32x8 gather8(const int32* address, int32x8 offset, int32x8 value, int32x8 mask)
+    {
+        return _mm256_mask_i32gather_epi32(value, reinterpret_cast<const int*>(address), offset, mask, 4);
+    }
+
+    static inline uint64x4 gather4(const uint64* address, int32x4 offset, uint64x4 value, uint64x4 mask)
+    {
+        return _mm256_mask_i32gather_epi64(value, reinterpret_cast<const long long*>(address), offset, mask, 8);
+    }
+
+    static inline int64x4 gather4(const int64* address, int32x4 offset, int64x4 value, int64x4 mask)
+    {
+        return _mm256_mask_i32gather_epi64(value, reinterpret_cast<const long long*>(address), offset, mask, 8);
+    }
+
 #else
 
     // 128 bit gather
@@ -179,6 +243,70 @@ namespace simd {
         auto s2 = address[get_component<2>(offset)];
         auto s3 = address[get_component<3>(offset)];
         return int64x4_set4(s0, s1, s2, s3);
+    }
+
+    // 128 bit masked gather
+
+    static inline float32x4 gather4(const float* address, int32x4 offset, float32x4 value, float32x4 mask)
+    {
+        return select(value, mask, gather4(address, offset));
+    }
+
+    static inline float64x2 gather2(const double* address, int32x4 offset, float64x2 value, float64x2 mask)
+    {
+        return select(value, mask, gather2(address, offset));
+    }
+
+    static inline uint32x4 gather4(const uint32* address, int32x4 offset, uint32x4 value, uint32x4 mask)
+    {
+        return select(value, mask, gather4(address, offset));
+    }
+
+    static inline int32x4 gather4(const int32* address, int32x4 offset, int32x4 value, int32x4 mask)
+    {
+        return select(value, mask, gather4(address, offset));
+    }
+
+    static inline uint64x2 gather2(const uint64* address, int32x4 offset, uint64x2 value, uint64x2 mask)
+    {
+        return select(value, mask, gather2(address, offset));
+    }
+
+    static inline int64x2 gather2(const int64* address, int32x4 offset, int64x2 value, int64x2 mask)
+    {
+        return select(value, mask, gather2(address, offset));
+    }
+
+    // 256 bit masked gather
+
+    static inline float32x8 gather8(const float* address, int32x8 offset, float32x8 value, float32x8 mask)
+    {
+        return select(value, mask, gather8(address, offset));
+    }
+
+    static inline float64x4 gather4(const double* address, int32x4 offset, float64x4 value, float64x4 mask)
+    {
+        return select(value, mask, gather4(address, offset));
+    }
+
+    static inline uint32x8 gather8(const uint32* address, int32x8 offset, uint32x8 value, uint32x8 mask)
+    {
+        return select(value, mask, gather8(address, offset));
+    }
+
+    static inline int32x8 gather8(const int32* address, int32x8 offset, int32x8 value, int32x8 mask)
+    {
+        return select(value, mask, gather8(address, offset));
+    }
+
+    static inline uint64x4 gather4(const uint64* address, int32x4 offset, uint64x4 value, uint64x4 mask)
+    {
+        return select(value, mask, gather4(address, offset));
+    }
+
+    static inline int64x4 gather4(const int64* address, int32x4 offset, int64x4 value, int64x4 mask)
+    {
+        return select(value, mask, gather4(address, offset));
     }
 
 #endif // defined(MANGO_ENABLE_AVX2)
