40d0d5eeca6dde0247dfff06c2e1ff63ad12e8ff
Jukka Liimatta <jukka.liimatta@liimatta.org>
2017-08-14 21:11:59 +0300

    Make ScalarAccessor convertible to Vector.

--- include/mango/math/vector.hpp
+++ include/mango/math/vector.hpp
@@ -776,6 +776,13 @@ namespace mango
     {
         VectorType m;
 
+        template <int VectorSize>
+        operator Vector<ScalarType, VectorSize> () const
+        {
+            ScalarType s = simd::get_component<Index>(m);
+            return Vector<ScalarType, VectorSize>(s);
+        }
+
         operator ScalarType () const
         {
             return simd::get_component<Index>(m);
