7e5808f2938eb11f5b95b3a5cc0381a582cf1b0a
t0rakka <jukka@liimatta.org>
2018-01-11 20:14:45 +0200

    Optimize SHA1 implementation.

--- source/mango/core/sha1.cpp
+++ source/mango/core/sha1.cpp
@@ -364,161 +364,145 @@ namespace {
 
 #else
 
-    /*
-     * SHA-1 hash in C
-     *
-     * Copyright (c) 2014 Project Nayuki
-     * https://www.nayuki.io/page/fast-sha1-hash-implementation-in-x86-assembly
-     *
-     * (MIT License)
-     * Permission is hereby granted, free of charge, to any person obtaining a copy of
-     * this software and associated documentation files (the "Software"), to deal in
-     * the Software without restriction, including without limitation the rights to
-     * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-     * the Software, and to permit persons to whom the Software is furnished to do so,
-     * subject to the following conditions:
-     * - The above copyright notice and this permission notice shall be included in
-     *   all copies or substantial portions of the Software.
-     * - The Software is provided "as is", without warranty of any kind, express or
-     *   implied, including but not limited to the warranties of merchantability,
-     *   fitness for a particular purpose and noninfringement. In no event shall the
-     *   authors or copyright holders be liable for any claim, damages or other
-     *   liability, whether in an action of contract, tort or otherwise, arising from,
-     *   out of or in connection with the Software or the use or other dealings in the
-     *   Software.
-     */
+    // ----------------------------------------------------------------------------------------
+    // Generic C++ SHA1
+    // ----------------------------------------------------------------------------------------
 
-#define SCHEDULE(i)  { \
-    uint32 temp = schedule[(i -  3) & 0xF] \
-                ^ schedule[(i -  8) & 0xF] \
-                ^ schedule[(i - 14) & 0xF] \
-                ^ schedule[(i - 16) & 0xF]; \
-    schedule[i & 0xF] = temp << 1 | temp >> 31; }
+#define SHW(index) \
+    uint32 x = (w[(index -  3) & 15] ^ \
+                w[(index -  8) & 15] ^ \
+                w[(index - 14) & 15] ^ \
+                w[(index - 16) & 15]); \
+    x = (x << 1) | (x >> 31); \
+    w[index & 15] = x;
 
-#define LOADSCHEDULE(i)  \
-    schedule[i] = uload32be(block + i * 4)
+#define PASS0(a, b, c, d, e, index) { \
+    uint32 x = uload32be(block + index * 4); \
+    w[index] = x; \
+    e = ((a << 5) | (a >> 27)) + ((b & c) | ((~b) & d)) + e + 0x5A827999 + x; } \
+    b = (b << 30) | (b >> 2)
 
-#define ROUNDTAIL(a, b, e, f, i, k) \
-    e += (a << 5 | a >> 27) + f + UINT32_C(k) + schedule[i & 0xF]; \
-    b = b << 30 | b >> 2
+#define PASS1(a, b, c, d, e, index) { \
+    SHW(index); \
+    e = ((a << 5) | (a >> 27)) + ((b & c) | ((~b) & d)) + e + 0x5A827999 + x; } \
+    b = (b << 30) | (b >> 2)
 
-#define ROUND0a(a, b, c, d, e, i)  LOADSCHEDULE(i);  ROUNDTAIL(a, b, e, ((b & c) | (~b & d))         , i, 0x5A827999);
-#define ROUND0b(a, b, c, d, e, i)  SCHEDULE(i);      ROUNDTAIL(a, b, e, ((b & c) | (~b & d))         , i, 0x5A827999);
-#define ROUND1(a, b, c, d, e, i)   SCHEDULE(i);      ROUNDTAIL(a, b, e, (b ^ c ^ d)                  , i, 0x6ED9EBA1);
-#define ROUND2(a, b, c, d, e, i)   SCHEDULE(i);      ROUNDTAIL(a, b, e, ((b & c) ^ (b & d) ^ (c & d)), i, 0x8F1BBCDC);
-#define ROUND3(a, b, c, d, e, i)   SCHEDULE(i);      ROUNDTAIL(a, b, e, (b ^ c ^ d)                  , i, 0xCA62C1D6);
+#define PASS2(a, b, c, d, e, index) { \
+    SHW(index); \
+    e = ((a << 5) | (a >> 27)) + (b ^ c ^ d) + e + 0x6ED9EBA1 + x; } \
+    b = (b << 30) | (b >> 2)
+
+#define PASS3(a, b, c, d, e, index) { \
+    SHW(index); \
+    e = ((a << 5) | (a >> 27)) + ((b & c) | (b & d) | (c & d)) + e + 0x8F1BBCDC + x; } \
+    b = (b << 30) | (b >> 2)
+
+#define PASS4(a, b, c, d, e, index) { \
+    SHW(index); \
+    e = ((a << 5) | (a >> 27)) + (b ^ c ^ d) + e + 0xCA62C1D6 + x; } \
+    b = (b << 30) | (b >> 2)
 
     void sha1_update(uint32 state[5], const uint8* block, int count)
     {
-        for (int i = 0; i < count; ++i)
-        {
-            uint32 a = state[0];
-            uint32 b = state[1];
-            uint32 c = state[2];
-            uint32 d = state[3];
-            uint32 e = state[4];
+        uint32 w[16];
 
-            uint32 schedule[16];
+        uint32 a = state[0];
+        uint32 b = state[1];
+        uint32 c = state[2];
+        uint32 d = state[3];
+        uint32 e = state[4];
 
-            ROUND0a(a, b, c, d, e,  0);
-            ROUND0a(e, a, b, c, d,  1);
-            ROUND0a(d, e, a, b, c,  2);
-            ROUND0a(c, d, e, a, b,  3);
-            ROUND0a(b, c, d, e, a,  4);
-            ROUND0a(a, b, c, d, e,  5);
-            ROUND0a(e, a, b, c, d,  6);
-            ROUND0a(d, e, a, b, c,  7);
-            ROUND0a(c, d, e, a, b,  8);
-            ROUND0a(b, c, d, e, a,  9);
-            ROUND0a(a, b, c, d, e, 10);
-            ROUND0a(e, a, b, c, d, 11);
-            ROUND0a(d, e, a, b, c, 12);
-            ROUND0a(c, d, e, a, b, 13);
-            ROUND0a(b, c, d, e, a, 14);
-            ROUND0a(a, b, c, d, e, 15);
-            ROUND0b(e, a, b, c, d, 16);
-            ROUND0b(d, e, a, b, c, 17);
-            ROUND0b(c, d, e, a, b, 18);
-            ROUND0b(b, c, d, e, a, 19);
-            ROUND1(a, b, c, d, e, 20);
-            ROUND1(e, a, b, c, d, 21);
-            ROUND1(d, e, a, b, c, 22);
-            ROUND1(c, d, e, a, b, 23);
-            ROUND1(b, c, d, e, a, 24);
-            ROUND1(a, b, c, d, e, 25);
-            ROUND1(e, a, b, c, d, 26);
-            ROUND1(d, e, a, b, c, 27);
-            ROUND1(c, d, e, a, b, 28);
-            ROUND1(b, c, d, e, a, 29);
-            ROUND1(a, b, c, d, e, 30);
-            ROUND1(e, a, b, c, d, 31);
-            ROUND1(d, e, a, b, c, 32);
-            ROUND1(c, d, e, a, b, 33);
-            ROUND1(b, c, d, e, a, 34);
-            ROUND1(a, b, c, d, e, 35);
-            ROUND1(e, a, b, c, d, 36);
-            ROUND1(d, e, a, b, c, 37);
-            ROUND1(c, d, e, a, b, 38);
-            ROUND1(b, c, d, e, a, 39);
-            ROUND2(a, b, c, d, e, 40);
-            ROUND2(e, a, b, c, d, 41);
-            ROUND2(d, e, a, b, c, 42);
-            ROUND2(c, d, e, a, b, 43);
-            ROUND2(b, c, d, e, a, 44);
-            ROUND2(a, b, c, d, e, 45);
-            ROUND2(e, a, b, c, d, 46);
-            ROUND2(d, e, a, b, c, 47);
-            ROUND2(c, d, e, a, b, 48);
-            ROUND2(b, c, d, e, a, 49);
-            ROUND2(a, b, c, d, e, 50);
-            ROUND2(e, a, b, c, d, 51);
-            ROUND2(d, e, a, b, c, 52);
-            ROUND2(c, d, e, a, b, 53);
-            ROUND2(b, c, d, e, a, 54);
-            ROUND2(a, b, c, d, e, 55);
-            ROUND2(e, a, b, c, d, 56);
-            ROUND2(d, e, a, b, c, 57);
-            ROUND2(c, d, e, a, b, 58);
-            ROUND2(b, c, d, e, a, 59);
-            ROUND3(a, b, c, d, e, 60);
-            ROUND3(e, a, b, c, d, 61);
-            ROUND3(d, e, a, b, c, 62);
-            ROUND3(c, d, e, a, b, 63);
-            ROUND3(b, c, d, e, a, 64);
-            ROUND3(a, b, c, d, e, 65);
-            ROUND3(e, a, b, c, d, 66);
-            ROUND3(d, e, a, b, c, 67);
-            ROUND3(c, d, e, a, b, 68);
-            ROUND3(b, c, d, e, a, 69);
-            ROUND3(a, b, c, d, e, 70);
-            ROUND3(e, a, b, c, d, 71);
-            ROUND3(d, e, a, b, c, 72);
-            ROUND3(c, d, e, a, b, 73);
-            ROUND3(b, c, d, e, a, 74);
-            ROUND3(a, b, c, d, e, 75);
-            ROUND3(e, a, b, c, d, 76);
-            ROUND3(d, e, a, b, c, 77);
-            ROUND3(c, d, e, a, b, 78);
-            ROUND3(b, c, d, e, a, 79);
+        PASS0(a, b, c, d, e, 0);
+        PASS0(e, a, b, c, d, 1);
+        PASS0(d, e, a, b, c, 2);
+        PASS0(c, d, e, a, b, 3);
+        PASS0(b, c, d, e, a, 4);
+        PASS0(a, b, c, d, e, 5);
+        PASS0(e, a, b, c, d, 6);
+        PASS0(d, e, a, b, c, 7);
+        PASS0(c, d, e, a, b, 8);
+        PASS0(b, c, d, e, a, 9);
+        PASS0(a, b, c, d, e, 10);
+        PASS0(e, a, b, c, d, 11);
+        PASS0(d, e, a, b, c, 12);
+        PASS0(c, d, e, a, b, 13);
+        PASS0(b, c, d, e, a, 14);
+        PASS0(a, b, c, d, e, 15);
 
-            state[0] += a;
-            state[1] += b;
-            state[2] += c;
-            state[3] += d;
-            state[4] += e;
+        PASS1(e, a, b, c, d, 16);
+        PASS1(d, e, a, b, c, 17);
+        PASS1(c, d, e, a, b, 18);
+        PASS1(b, c, d, e, a, 19);
 
-            block += 64;
-        }
+        PASS2(a, b, c, d, e, 20);
+        PASS2(e, a, b, c, d, 21);
+        PASS2(d, e, a, b, c, 22);
+        PASS2(c, d, e, a, b, 23);
+        PASS2(b, c, d, e, a, 24);
+        PASS2(a, b, c, d, e, 25);
+        PASS2(e, a, b, c, d, 26);
+        PASS2(d, e, a, b, c, 27);
+        PASS2(c, d, e, a, b, 28);
+        PASS2(b, c, d, e, a, 29);
+        PASS2(a, b, c, d, e, 30);
+        PASS2(e, a, b, c, d, 31);
+        PASS2(d, e, a, b, c, 32);
+        PASS2(c, d, e, a, b, 33);
+        PASS2(b, c, d, e, a, 34);
+        PASS2(a, b, c, d, e, 35);
+        PASS2(e, a, b, c, d, 36);
+        PASS2(d, e, a, b, c, 37);
+        PASS2(c, d, e, a, b, 38);
+        PASS2(b, c, d, e, a, 39);
+
+        PASS3(a, b, c, d, e, 40);
+        PASS3(e, a, b, c, d, 41);
+        PASS3(d, e, a, b, c, 42);
+        PASS3(c, d, e, a, b, 43);
+        PASS3(b, c, d, e, a, 44);
+        PASS3(a, b, c, d, e, 45);
+        PASS3(e, a, b, c, d, 46);
+        PASS3(d, e, a, b, c, 47);
+        PASS3(c, d, e, a, b, 48);
+        PASS3(b, c, d, e, a, 49);
+        PASS3(a, b, c, d, e, 50);
+        PASS3(e, a, b, c, d, 51);
+        PASS3(d, e, a, b, c, 52);
+        PASS3(c, d, e, a, b, 53);
+        PASS3(b, c, d, e, a, 54);
+        PASS3(a, b, c, d, e, 55);
+        PASS3(e, a, b, c, d, 56);
+        PASS3(d, e, a, b, c, 57);
+        PASS3(c, d, e, a, b, 58);
+        PASS3(b, c, d, e, a, 59);
+
+        PASS4(a, b, c, d, e, 60);
+        PASS4(e, a, b, c, d, 61);
+        PASS4(d, e, a, b, c, 62);
+        PASS4(c, d, e, a, b, 63);
+        PASS4(b, c, d, e, a, 64);
+        PASS4(a, b, c, d, e, 65);
+        PASS4(e, a, b, c, d, 66);
+        PASS4(d, e, a, b, c, 67);
+        PASS4(c, d, e, a, b, 68);
+        PASS4(b, c, d, e, a, 69);
+        PASS4(a, b, c, d, e, 70);
+        PASS4(e, a, b, c, d, 71);
+        PASS4(d, e, a, b, c, 72);
+        PASS4(c, d, e, a, b, 73);
+        PASS4(b, c, d, e, a, 74);
+        PASS4(a, b, c, d, e, 75);
+        PASS4(e, a, b, c, d, 76);
+        PASS4(d, e, a, b, c, 77);
+        PASS4(c, d, e, a, b, 78);
+        PASS4(b, c, d, e, a, 79);
+
+        state[0] += a;
+        state[1] += b;
+        state[2] += c;
+        state[3] += d;
+        state[4] += e;
     }
-
-#undef SCHEDULE
-#undef LOADSCHEDULE
-#undef ROUNDTAIL
-#undef ROUND0a
-#undef ROUND0b
-#undef ROUND1
-#undef ROUND2
-#undef ROUND3
 
 #endif
 
