1a33f3431b0116b20221276a183656a81591397f
Jukka Liimatta <jukka.liimatta@virelabs.com>
2018-09-01 13:43:35 +0300

    Add PNG image decoder.

--- source/mango/image/image.cpp
+++ source/mango/image/image.cpp
@@ -29,6 +29,7 @@ namespace mango
     void registerImageDecoderASTC();
     void registerImageDecoderZPNG();
     void registerImageDecoderSGI();
+    void registerImageDecoderPNM();
 
     class ImageServer
     {
@@ -55,6 +56,7 @@ namespace mango
             registerImageDecoderASTC();
             registerImageDecoderZPNG();
             registerImageDecoderSGI();
+            registerImageDecoderPNM();
         }
 
         ~ImageServer()
