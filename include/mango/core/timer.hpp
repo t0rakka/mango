5cad29ce3e4fee47dc949341669c0efb05978d4a
Jukka Liimatta <jukka.liimatta@virelabs.com>
2019-04-26 20:19:22 +0300

    Whitespace patrol.

--- include/mango/core/timer.hpp
+++ include/mango/core/timer.hpp
@@ -1,6 +1,6 @@
 /*
     MANGO Multimedia Development Platform
-    Copyright (C) 2012-2016 Twilight Finland 3D Oy Ltd. All rights reserved.
+    Copyright (C) 2012-2019 Twilight Finland 3D Oy Ltd. All rights reserved.
 */
 #pragma once
 
@@ -58,11 +58,11 @@ namespace mango
 
     struct LocalTime
     {
-        u16  year;	 // [....]
-        u16  month;	 // [1, 12]
-        u16  day;	 // [1, 31]
+        u16  year;   // [....]
+        u16  month;  // [1, 12]
+        u16  day;    // [1, 31]
         u16  wday;   // [0, 6]
-        u16  hour;	 // [0, 23]
+        u16  hour;   // [0, 23]
         u16  minute; // [0, 59]
         u16  second; // [0, 60]
     };
