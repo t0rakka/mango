6fe7dee982bb5035759168f5a3dcc47ccd97999a
Jukka Liimatta <jukka.liimatta@virelabs.com>
2019-08-07 00:12:41 +0300

    std aligned memory allocator.

--- include/mango/core/memory.hpp
+++ include/mango/core/memory.hpp
@@ -149,4 +149,107 @@ namespace mango {
         }
     };
 
+    // -----------------------------------------------------------------------
+    // aligned (std) memory allocator
+    // -----------------------------------------------------------------------
+
+    template <typename T, size_t ALIGNMENT>
+    class AlignedAllocator
+    {
+    public:
+        typedef T               value_type;
+        typedef T*              pointer;
+        typedef T&              reference;
+        typedef const T*        const_pointer;
+        typedef const T&        const_reference;
+        typedef std::size_t     size_type;
+        typedef std::ptrdiff_t  difference_type;
+
+        AlignedAllocator()
+        {
+        }
+
+        AlignedAllocator(const AlignedAllocator& allocator)
+        {
+            MANGO_UNREFERENCED_PARAMETER(allocator);
+        }
+
+        template <class U>
+        AlignedAllocator(const AlignedAllocator<U, ALIGNMENT>& allocator)
+        {
+            MANGO_UNREFERENCED_PARAMETER(allocator);
+        }
+
+        ~AlignedAllocator()
+        {
+        }
+
+        pointer address(reference x) const
+        {
+            return &x;
+        }
+
+        const_pointer address(const_reference x) const
+        {
+            return &x;
+        }
+
+        pointer allocate(size_type n, std::allocator<void>::const_pointer hint = 0)
+        {
+            MANGO_UNREFERENCED_PARAMETER(hint);
+            void* s = aligned_malloc(n * sizeof(T), ALIGNMENT);
+            return reinterpret_cast<pointer>(s);
+        }
+
+        void deallocate(pointer p, size_type n)
+        {
+            MANGO_UNREFERENCED_PARAMETER(n);
+            aligned_free(p);
+        }
+
+        void construct(pointer p, const_reference value)
+        {
+            new(p) T(value);
+        }
+
+        void destroy(pointer p)
+        {
+            p->~T();
+        }
+
+        template <class U, class... Args>
+        void construct(U* p, Args&&... args)
+        {
+            new(p) T(args...);
+        }
+
+        template <class U>
+        void destroy (U* p)
+        {
+            p->~T();
+        }
+
+        size_type max_size() const
+        {
+            return std::numeric_limits<size_type>::max() / sizeof(T);
+        }
+
+        template <typename U>
+        struct rebind
+        {
+            typedef AlignedAllocator<U, ALIGNMENT> other;
+        };
+
+        bool operator == (AlignedAllocator const& allocator)
+        {
+            MANGO_UNREFERENCED_PARAMETER(allocator);
+            return true;
+        }
+
+        bool operator != (AlignedAllocator const& allocator)
+        {
+            return !operator == (allocator);
+        }
+    };
+
 } // namespace mango
