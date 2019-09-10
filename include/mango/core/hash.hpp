db6aa9874bd76ea48a9bde5bc3ecf706a62c315d
t0rakka <jukka@liimatta.org>
2019-09-10 18:38:34 +0300

    Add XX3 hashing.

--- include/mango/core/hash.hpp
+++ include/mango/core/hash.hpp
@@ -51,13 +51,15 @@ namespace mango
         return std::memcmp(a.data, b.data, sizeof(Hash<T, S>)) > 0;
     }
 
-    using MD5 = Hash<u32, 4>;
-    using SHA1 = Hash<u32, 5>;
-    using SHA2 = Hash<u32, 8>;
-
     // -----------------------------------------------------------------------
     // hashing functions
     // -----------------------------------------------------------------------
+
+    using MD5 = Hash<u32, 4>;
+    using SHA1 = Hash<u32, 5>;
+    using SHA2 = Hash<u32, 8>;
+    using XX3HASH64 = u64;
+    using XX3HASH128 = Hash<u64, 2>;
 
     MD5 md5(Memory memory);
     SHA1 sha1(Memory memory);
@@ -65,5 +67,12 @@ namespace mango
 
     u32 xxhash32(u32 seed, Memory memory);
     u64 xxhash64(u64 seed, Memory memory);
+
+    // WARNING!
+    // The experimental SIMD enhanced hashing functions are NON-STABLE; the hash values might
+    // change when the xx3-hash is upgraded. This notice will be removed after the API is stable.
+
+    XX3HASH64 xx3hash64(u64 seed, Memory memory);
+    XX3HASH128 xx3hash128(u64 seed, Memory memory);
 
 } // namespace mango
--- source/mango/core/hash.cpp
+++ source/mango/core/hash.cpp
@@ -3,6 +3,8 @@
     Copyright (C) 2012-2019 Twilight Finland 3D Oy Ltd. All rights reserved.
 */
 #include <mango/core/hash.hpp>
+
+#define XXH_STATIC_LINKING_ONLY
 #include "../../external/zstd/common/xxhash.h"
 
 namespace mango {
@@ -15,6 +17,17 @@ namespace mango {
     u64 xxhash64(u64 seed, Memory memory)
     {
         return XXH64(memory.address, memory.size, seed);
+    }
+
+    XX3HASH64 xx3hash64(u64 seed, Memory memory)
+    {
+        return XXH3_64bits_withSeed(memory.address, memory.size, seed);
+    }
+
+    XX3HASH128 xx3hash128(u64 seed, Memory memory)
+    {
+        const XXH128_hash_t hash = XXH128(memory.address, memory.size, seed);
+        return { hash.low64, hash.high64 };
     }
 
 } // namespace mango
