117171a96e330b4956344eef9124aba1ec20bbc2
Jukka Liimatta <jukka.liimatta@virelabs.com>
2019-09-26 14:54:22 +0300

    Make refcount atomic.

--- source/mango/core/object.cpp
+++ source/mango/core/object.cpp
@@ -1,6 +1,6 @@
 /*
     MANGO Multimedia Development Platform
-    Copyright (C) 2012-2017 Twilight Finland 3D Oy Ltd. All rights reserved.
+    Copyright (C) 2012-2019 Twilight Finland 3D Oy Ltd. All rights reserved.
 */
 #include <mango/core/object.hpp>
 
@@ -19,7 +19,8 @@ namespace mango
     int RefCounted::release()
     {
         const int count = --m_count;
-        if (!count) {
+        if (!count)
+        {
             delete this;
         }
 
