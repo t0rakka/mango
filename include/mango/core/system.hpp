ec4abb25a6420d5b1aa7bb68bf6c66e3755a12f4
t0rakka <jukka@liimatta.org>
2020-03-24 12:23:39 +0200

    Make debugPrint synchronous.
    Slower, but never misses timings.

--- include/mango/core/system.hpp
+++ include/mango/core/system.hpp
@@ -1,6 +1,6 @@
 /*
     MANGO Multimedia Development Platform
-    Copyright (C) 2012-2019 Twilight Finland 3D Oy Ltd. All rights reserved.
+    Copyright (C) 2012-2020 Twilight Finland 3D Oy Ltd. All rights reserved.
 */
 #pragma once
 
@@ -13,8 +13,14 @@ namespace mango
 
     std::string getSystemInfo();
 
+    // --------------------------------------------------------------
+    // debugPrint()
+    // --------------------------------------------------------------
+
 #ifdef MANGO_ENABLE_DEBUG_PRINT
-    #define debugPrint(...) std::printf(__VA_ARGS__)
+    #define debugPrint(...) \
+        std::printf(__VA_ARGS__); \
+        std::fflush(stdout)
 #else
     #define debugPrint(...)
 #endif
