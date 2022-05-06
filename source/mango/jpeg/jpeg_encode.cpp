7dc0255891997ba998dd8f3c4a2a04014e01ff0c
t0rakka <jukka@liimatta.org>
2022-05-06 17:02:51 +0300

    Fix encoder.

--- source/mango/jpeg/jpeg_encode.cpp
+++ source/mango/jpeg/jpeg_encode.cpp
@@ -180,7 +180,8 @@ namespace
     static inline
     u8* flushStuffedBytes(u8* output, DataType code)
     {
-        if (u64_has_zero_byte(code))
+        //if (u64_has_zero_byte(code))
+        if (code & 0x8080808080808080ull & ~(code + 0x0101010101010101ull))
         {
             output = writeStuffedBytes(output, code, 8);
         }
@@ -197,7 +198,8 @@ namespace
     static inline
     u8* flushStuffedBytes(u8* output, DataType code)
     {
-        if (u32_has_zero_byte(code))
+        //if (u32_has_zero_byte(code))
+        if (code & 0x80808080 & ~(code + 0x01010101))
         {
             output = writeStuffedBytes(output, code, 4);
         }
