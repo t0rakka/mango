c955c7544476e2bcf0d98091f9f6ac5b73a25fe0
t0rakka <jukka@liimatta.org>
2022-09-28 11:48:17 +0300

    Fix compiler warnings.

--- source/mango/image/image_png.cpp
+++ source/mango/image/image_png.cpp
@@ -953,10 +953,20 @@ namespace
         uint32x2x4_t tmp;
 
         tmp = vld4_lane_u32(reinterpret_cast<u32 *>(scan), tmp, 0);
-        uint8x8x4_t a = *(uint8x8x4_t*) &tmp; // !!!
+
+        uint8x8x4_t a;
+        a.val[0] = vreinterpret_u8_u32(tmp.val[0]);
+        a.val[1] = vreinterpret_u8_u32(tmp.val[1]);
+        a.val[2] = vreinterpret_u8_u32(tmp.val[2]);
+        a.val[3] = vreinterpret_u8_u32(tmp.val[3]);
 
         tmp = vld4_lane_u32(reinterpret_cast<const u32 *>(prev), tmp, 0);
-        uint8x8x4_t b = *(uint8x8x4_t *) &tmp; // !!!
+
+        uint8x8x4_t b;
+        b.val[0] = vreinterpret_u8_u32(tmp.val[0]);
+        b.val[1] = vreinterpret_u8_u32(tmp.val[1]);
+        b.val[2] = vreinterpret_u8_u32(tmp.val[2]);
+        b.val[3] = vreinterpret_u8_u32(tmp.val[3]);
 
         value.val[0] = paeth(value.val[3], b.val[0],     last, a.val[0]);
         value.val[1] = paeth(value.val[0], b.val[1], b.val[0], a.val[1]);
@@ -1707,9 +1717,9 @@ namespace
         {
             const uint16x4x3_t rgb = vld3_u16(reinterpret_cast<const u16*>(src));
             uint16x4x4_t rgba;
-            rgba.val[0] = vreinterpret_u16_u8(vreinterpret_u8_u16(vrev16_u8(rgb.val[0])));
-            rgba.val[1] = vreinterpret_u16_u8(vreinterpret_u8_u16(vrev16_u8(rgb.val[1])));
-            rgba.val[2] = vreinterpret_u16_u8(vreinterpret_u8_u16(vrev16_u8(rgb.val[2])));
+            rgba.val[0] = vreinterpret_u16_u8(vrev16_u8(vreinterpret_u8_u16(rgb.val[0])));
+            rgba.val[1] = vreinterpret_u16_u8(vrev16_u8(vreinterpret_u8_u16(rgb.val[1])));
+            rgba.val[2] = vreinterpret_u16_u8(vrev16_u8(vreinterpret_u8_u16(rgb.val[2])));
             rgba.val[3] = vdup_n_u16(0xffff);
             vst4_u16(dest, rgba);
             src += 24;
