65a9dbef49f88de17656d868edae9578da2716c4
t0rakka <jukka@liimatta.org>
2022-11-02 19:52:03 +0200

    Add AVIF encoder.

--- source/mango/image/image_avif.cpp
+++ source/mango/image/image_avif.cpp
@@ -7,7 +7,7 @@
 
 #if defined(MANGO_ENABLE_AVIF)
 
-//#include <libavif.h>
+#include <avif/avif.h>
 
 namespace
 {
@@ -82,8 +82,79 @@ namespace
 
     ImageEncodeStatus imageEncode(Stream& output, const Surface& surface, const ImageEncodeOptions& options)
     {
-        // TODO
         ImageEncodeStatus status;
+
+        int width = surface.width;
+        int height = surface.height;
+
+        avifImage* image = avifImageCreate(width, height, 8, AVIF_PIXEL_FORMAT_YUV444);
+        if (!image)
+        {
+            status.setError("[ImageEncoder.AVIF] avifImageCreate FAILED.");
+            return status;
+        }
+
+        avifRGBImage rgb;
+        std::memset(&rgb, 0, sizeof(rgb));
+
+        avifRGBImageSetDefaults(&rgb, image);
+        avifRGBImageAllocatePixels(&rgb);
+
+        Surface temp(width, height, Format(32, Format::UNORM, Format::RGBA, 8, 8, 8, 8), rgb.rowBytes, rgb.pixels);
+        temp.blit(0, 0, surface);
+
+        avifResult convertResult = avifImageRGBToYUV(image, &rgb);
+        if (convertResult != AVIF_RESULT_OK)
+        {
+            avifRGBImageFreePixels(&rgb);
+            avifImageDestroy(image);
+            status.setError("[ImageEncoder.AVIF] avifImageRGBToYUV FAILED.");
+            return status;
+        }
+
+        avifEncoder* encoder = avifEncoderCreate();
+        if (!encoder)
+        {
+            avifRGBImageFreePixels(&rgb);
+            avifImageDestroy(image);
+            status.setError("[ImageEncoder.AVIF] avifEncoderCreate FAILED.");
+            return status;
+        }
+
+        encoder->maxThreads = int(std::thread::hardware_concurrency());
+
+        avifResult addImageResult = avifEncoderAddImage(encoder, image, 1, AVIF_ADD_IMAGE_FLAG_SINGLE);
+        if (addImageResult != AVIF_RESULT_OK)
+        {
+            avifRGBImageFreePixels(&rgb);
+            avifEncoderDestroy(encoder);
+            avifImageDestroy(image);
+            status.setError("[ImageEncoder.AVIF] avifEncoderAddImage FAILED.");
+            return status;
+        }
+
+        avifRWData avifOutput = AVIF_DATA_EMPTY;
+        avifResult finishResult = avifEncoderFinish(encoder, &avifOutput);
+        if (finishResult != AVIF_RESULT_OK)
+        {
+            avifRWDataFree(&avifOutput);
+            avifRGBImageFreePixels(&rgb);
+            avifEncoderDestroy(encoder);
+            avifImageDestroy(image);
+            status.setError("[ImageEncoder.AVIF] avifEncoderFinish FAILED.");
+            return status;
+        }
+
+        // TODO: debugPrint
+        printf("Encode success: %zu total bytes\n", avifOutput.size);
+
+        output.write(avifOutput.data, avifOutput.size);
+
+        avifRWDataFree(&avifOutput);
+        avifRGBImageFreePixels(&rgb);
+        avifEncoderDestroy(encoder);
+        avifImageDestroy(image);
+
         return status;
     }
 
@@ -96,11 +167,10 @@ namespace mango::image
     {
         // TODO
         MANGO_UNREFERENCED(createInterface);
-        MANGO_UNREFERENCED(imageEncode);
         /*
         registerImageDecoder(createInterface, ".xxx");
-        registerImageEncoder(imageEncode, ".xxx");
         */
+        registerImageEncoder(imageEncode, ".avif");
     }
 
 } // namespace mango::image
