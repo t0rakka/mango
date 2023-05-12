7289ad3657f5e4d36fe784c65c24782161c1837b
t0rakka <jukka@liimatta.org>
2023-05-12 17:04:57 +0300

    Improve system info message.

--- source/mango/core/system.cpp
+++ source/mango/core/system.cpp
@@ -1,6 +1,6 @@
 /*
     MANGO Multimedia Development Platform
-    Copyright (C) 2012-2021 Twilight Finland 3D Oy Ltd. All rights reserved.
+    Copyright (C) 2012-2023 Twilight Finland 3D Oy Ltd. All rights reserved.
 */
 #include <mango/core/system.hpp>
 #include <mango/core/cpuinfo.hpp>
@@ -42,7 +42,7 @@ namespace mango
     {
         std::stringstream info;
 
-        info << "Platform: \"" << MANGO_PLATFORM_NAME << "\"" << std::endl;
+        info << "Platform: \"" << MANGO_PLATFORM_NAME << "\", ";
 
         info << "CPU: \"" << MANGO_CPU_NAME << "\", ";
 #ifdef MANGO_CPU_64BIT
@@ -71,151 +71,169 @@ namespace mango
 
         u64 flags = getCPUFlags();
 
-        info << "Detected CPU Features: ";
-        if (!flags) info << "N/A";
-        if (flags & INTEL_MMX) info << "MMX ";
-        if (flags & INTEL_MMX_PLUS) info << "MMX+ ";
-        if (flags & INTEL_SSE) info << "SSE ";
-        if (flags & INTEL_SSE2) info << "SSE2 ";
-        if (flags & INTEL_SSE3) info << "SSE3 ";
-        if (flags & INTEL_SSSE3) info << "SSSE3 ";
-        if (flags & INTEL_SSE4_1) info << "SSE4.1 ";
-        if (flags & INTEL_SSE4_2) info << "SSE4.2 ";
-        if (flags & INTEL_SSE4A) info << "SSE4A ";
-        if (flags & INTEL_XOP) info << "XOP ";
-        if (flags & INTEL_3DNOW) info << "3DNOW ";
-        if (flags & INTEL_3DNOW_EXT) info << "3DNOW+ ";
-        if (flags & INTEL_AVX) info << "AVX ";
-        if (flags & INTEL_AVX2) info << "AVX2 ";
-        if (flags & INTEL_AES) info << "AES ";
-        if (flags & INTEL_CLMUL) info << "CLMUL ";
-        if (flags & INTEL_FMA3) info << "FMA3 ";
-        if (flags & INTEL_MOVBE) info << "MOVBE ";
-        if (flags & INTEL_POPCNT) info << "POPCNT ";
-        if (flags & INTEL_F16C) info << "F16C ";
-        if (flags & INTEL_RDRAND) info << "RDRAND ";
-        if (flags & INTEL_CMOV) info << "CMOV ";
-        if (flags & INTEL_CMPXCHG16B) info << "CMPXCHG16B ";
-        if (flags & INTEL_FMA4) info << "FMA4 ";
-        if (flags & INTEL_BMI1) info << "BMI1 ";
-        if (flags & INTEL_BMI2) info << "BMI2 ";
-        if (flags & INTEL_SHA) info << "SHA ";
-        if (flags & INTEL_LZCNT) info << "LZCNT ";
-        if (flags & INTEL_AVX512F) info << "AVX512F ";
-        if (flags & INTEL_AVX512PFI) info << "AVX512PFI ";
-        if (flags & INTEL_AVX512ERI) info << "AVX512ERI ";
-        if (flags & INTEL_AVX512CDI) info << "AVX512CDI ";
-        if (flags & INTEL_AVX512BW) info << "AVX512BW ";
-        if (flags & INTEL_AVX512VL) info << "AVX512VL ";
-        if (flags & INTEL_AVX512DQ) info << "AVX512DQ ";
-        if (flags & INTEL_AVX512IFMA) info << "AVX512IFMA ";
-        if (flags & INTEL_AVX512VBMI) info << "AVX512VBMI ";
-        if (flags & INTEL_AVX512FP16) info << "AVX512FP16 ";
-        if (flags & ARM_NEON) info << "NEON ";
-        if (flags & ARM_CRC32) info << "CRC32 ";
-        if (flags & ARM_AES) info << "AES ";
-        if (flags & ARM_SHA1) info << "SHA1 ";
-        if (flags & ARM_SHA2) info << "SHA2 ";
-        if (flags & ARM_PMULL) info << "PMULL ";
+        const u64 avx512_mask =
+            INTEL_AVX512F |
+            INTEL_AVX512PFI |
+            INTEL_AVX512ERI |
+            INTEL_AVX512CDI |
+            INTEL_AVX512BW |
+            INTEL_AVX512VL |
+            INTEL_AVX512DQ |
+            INTEL_AVX512IFMA |
+            INTEL_AVX512VBMI |
+            INTEL_AVX512FP16;
+
+        info << "CPU Features:";
+        if (!flags) info << " N/A";
+        if (flags & INTEL_XOP) info << " XOP";
+        if (flags & INTEL_AES) info << " AES";
+        if (flags & INTEL_CLMUL) info << " CLMUL";
+        if (flags & INTEL_FMA3) info << " FMA3";
+        if (flags & INTEL_MOVBE) info << " MOVBE";
+        if (flags & INTEL_POPCNT) info << " POPCNT";
+        if (flags & INTEL_F16C) info << " F16C";
+        if (flags & INTEL_RDRAND) info << " RDRAND";
+        if (flags & INTEL_CMOV) info << " CMOV";
+        if (flags & INTEL_CMPXCHG16B) info << " CMPXCHG16B";
+        if (flags & INTEL_FMA4) info << " FMA4";
+        if (flags & INTEL_BMI1) info << " BMI1";
+        if (flags & INTEL_BMI2) info << " BMI2";
+        if (flags & INTEL_SHA) info << " SHA";
+        if (flags & INTEL_LZCNT) info << " LZCNT";
+        if (flags & ARM_CRC32) info << " CRC32";
+        if (flags & ARM_AES) info << " AES";
+        if (flags & ARM_SHA1) info << " SHA1";
+        if (flags & ARM_SHA2) info << " SHA2";
+        if (flags & ARM_PMULL) info << " PMULL";
         info << std::endl;
 
-        // NOTE: These are flags used to compile the mango library and have no relevance to
-        //       compiler settings used in the client application code. 
-        info << "Compiled SIMD Features: ";
+        info << "SIMD Features:";
+        if (flags & INTEL_3DNOW) info << " 3DNOW";
+        if (flags & INTEL_3DNOW_EXT) info << " 3DNOW+";
+        if (flags & INTEL_MMX) info << " MMX";
+        if (flags & INTEL_MMX_PLUS) info << " MMX+";
+        if (flags & INTEL_SSE) info << " SSE";
+        if (flags & INTEL_SSE2) info << " SSE2";
+        if (flags & INTEL_SSE3) info << " SSE3";
+        if (flags & INTEL_SSSE3) info << " SSSE3";
+        if (flags & INTEL_SSE4_1) info << " SSE4.1";
+        if (flags & INTEL_SSE4_2) info << " SSE4.2";
+        if (flags & INTEL_SSE4A) info << " SSE4A";
+        if (flags & INTEL_AVX) info << " AVX";
+        if (flags & INTEL_AVX2) info << " AVX2";
+        if (flags & avx512_mask)
+        {
+            info << " AVX512:";
+            if (flags & INTEL_AVX512F) info << "F|";
+            if (flags & INTEL_AVX512PFI) info << "PFI|";
+            if (flags & INTEL_AVX512ERI) info << "ERI|";
+            if (flags & INTEL_AVX512CDI) info << "CDI|";
+            if (flags & INTEL_AVX512BW) info << "BW|";
+            if (flags & INTEL_AVX512VL) info << "VL|";
+            if (flags & INTEL_AVX512DQ) info << "DQ|";
+            if (flags & INTEL_AVX512IFMA) info << "IFMA|";
+            if (flags & INTEL_AVX512VBMI) info << "VBMI|";
+            if (flags & INTEL_AVX512FP16) info << "FP16|";
+            info.seekp(-1, std::ios_base::cur);
+        }
+        if (flags & ARM_NEON) info << " NEON";
+        info << std::endl;
+
+        info << "Compiler Flags:";
 
 #if defined(MANGO_ENABLE_SIMD)
 
     #if defined(MANGO_ENABLE_SSE)
-        info << "SSE ";
+        info << " SSE";
     #endif
 
     #if defined(MANGO_ENABLE_SSE2)
-        info << "SSE2 ";
+        info << " SSE2";
     #endif
 
     #if defined(MANGO_ENABLE_SSE3)
-        info << "SSE3 ";
+        info << " SSE3";
     #endif
 
     #if defined(MANGO_ENABLE_SSSE3)
-        info << "SSSE3 ";
+        info << " SSSE3";
     #endif
 
     #if defined(MANGO_ENABLE_SSE4_1)
-        info << "SSE4.1 ";
+        info << " SSE4.1";
     #endif
 
     #if defined(MANGO_ENABLE_SSE4_2)
-        info << "SSE4.2 ";
+        info << " SSE4.2";
     #endif
 
     #if defined(MANGO_ENABLE_AVX)
-        info << "AVX ";
+        info << " AVX";
     #endif
 
     #if defined(MANGO_ENABLE_AVX2)
-        info << "AVX2 ";
+        info << " AVX2";
     #endif
 
     #if defined(MANGO_ENABLE_AVX512)
-        info << "AVX512 ";
+        info << " AVX512";
     #endif
 
     #if defined(MANGO_ENABLE_XOP)
-        info << "XOP ";
+        info << " XOP";
     #endif
 
     #if defined(MANGO_ENABLE_FMA4)
-        info << "FMA4 ";
+        info << " FMA4";
     #endif
 
     #if defined(MANGO_ENABLE_FMA3)
-        info << "FMA3 ";
+        info << " FMA3";
     #endif
 
     #if defined(MANGO_ENABLE_POPCNT)
-        info << "POPCNT ";
+        info << " POPCNT";
     #endif
 
     #if defined(MANGO_ENABLE_INTEL_F16C)
-        info << "F16C ";
+        info << " F16C";
     #endif
 
     #if defined(MANGO_ENABLE_BMI_32BIT) || defined(MANGO_ENABLE_BMI_64BIT)
-        info << "BMI ";
+        info << " BMI";
     #endif
 
     #if defined(MANGO_ENABLE_BMI2_32BIT) || defined(MANGO_ENABLE_BMI2_64BIT)
-        info << "BMI2 ";
+        info << " BMI2";
     #endif
 
     #if defined(MANGO_ENABLE_LZCNT_32BIT) || defined(MANGO_ENABLE_LZCNT_64BIT)
-        info << "LZCNT ";
+        info << " LZCNT";
     #endif
 
     #if defined(MANGO_ENABLE_AES)
-        info << "AES ";
+        info << " AES";
     #endif
 
     #if defined(MANGO_ENABLE_SHA)
-        info << "SHA ";
+        info << " SHA";
     #endif
 
     #if defined(MANGO_ENABLE_NEON)
-        info << "NEON ";
+        info << " NEON";
     #endif
 
     #if defined(__ARM_FEATURE_CRC32)
-        info << "CRC32 ";
+        info << " CRC32";
     #endif
 
     #if defined(__ARM_FEATURE_CRYPTO)
-        info << "CRYPTO ";
+        info << " CRYPTO";
     #endif
 
 #else
 
-        info << "N/A";
+        info << " N/A";
 
 #endif
 
