bbcaca2052c1f56cb9b79976f00a2b5820e65e20
t0rakka <jukka@liimatta.org>
2024-02-16 05:48:15 +0200

    Add more ways to create GLSL shaders.

--- include/mango/opengl/utilities.hpp
+++ include/mango/opengl/utilities.hpp
@@ -1,9 +1,12 @@
 /*
     MANGO Multimedia Development Platform
-    Copyright (C) 2012-2023 Twilight Finland 3D Oy Ltd. All rights reserved.
+    Copyright (C) 2012-2024 Twilight Finland 3D Oy Ltd. All rights reserved.
 */
 #pragma once
 
+#include <vector>
+#include <string_view>
+#include <mango/filesystem/filesystem.hpp>
 #include <mango/opengl/context.hpp>
 
 #ifndef MANGO_OPENGL_CONTEXT_NONE
@@ -13,8 +16,12 @@ namespace mango::opengl
 
     GLint getCompileStatus(GLuint shader);
     GLint getLinkStatus(GLuint program);
-    GLuint createShader(GLenum type, const char* source);
-    GLuint createProgram(const char* vertexShaderSource, const char* fragmentShaderSource);
+    GLuint createShader(GLenum type, std::string_view source);
+    GLuint createShader(GLenum type, std::vector<std::string_view> sources);
+    GLuint createProgram(std::string_view vertexShaderSource, std::string_view fragmentShaderSource);
+    GLuint createProgram(std::vector<std::string_view> vertexShaderSources, std::vector<std::string_view> fragmentShaderSources);
+    GLuint createProgram(filesystem::Path& path, const std::string& vertexShaderFilename, const std::string& fragmentShaderFilename);
+    GLuint createProgram(filesystem::Path& path, const std::vector<std::string>& vertexShaderFilenames, const std::vector<std::string>& fragmentShaderFilenames);
 
 } // namespace mango::opengl
 
--- source/mango/opengl/utilities.cpp
+++ source/mango/opengl/utilities.cpp
@@ -51,18 +51,41 @@ namespace mango::opengl
         return success;
     }
 
-    GLuint createShader(GLenum type, const char* source)
+    GLuint createShader(GLenum type, std::string_view source)
     {
         GLuint shader = glCreateShader(type);
-        glShaderSource(shader, 1, &source, NULL);
-        glCompileShader(shader);
 
+        const GLchar* data = source.data();
+        GLint length = source.length();
+
+        glShaderSource(shader, 1, &data, &length);
+        glCompileShader(shader);
         getCompileStatus(shader);
 
         return shader;
     }
 
-    GLuint createProgram(const char* vertexShaderSource, const char* fragmentShaderSource)
+    GLuint createShader(GLenum type, std::vector<std::string_view> sources)
+    {
+        GLuint shader = glCreateShader(type);
+
+        std::vector<const GLchar*> strings;
+        std::vector<GLint> lengths;
+
+        for (auto view : sources)
+        {
+            strings.emplace_back(view.data());
+            lengths.emplace_back(view.length());
+        }
+
+        glShaderSource(shader, sources.size(), strings.data(), lengths.data());
+        glCompileShader(shader);
+        getCompileStatus(shader);
+
+        return shader;
+    }
+
+    GLuint createProgram(std::string_view vertexShaderSource, std::string_view fragmentShaderSource)
     {
         GLuint vertex = createShader(GL_VERTEX_SHADER, vertexShaderSource);
         GLuint fragment = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
@@ -78,6 +101,90 @@ namespace mango::opengl
         glDeleteShader(fragment);
 
         return program;
+    }
+
+    GLuint createProgram(std::vector<std::string_view> vertexShaderSources, std::vector<std::string_view> fragmentShaderSources)
+    {
+        GLuint vertex = createShader(GL_VERTEX_SHADER, vertexShaderSources);
+        GLuint fragment = createShader(GL_FRAGMENT_SHADER, fragmentShaderSources);
+
+        GLuint program = glCreateProgram();
+        glAttachShader(program, vertex);
+        glAttachShader(program, fragment);
+        glLinkProgram(program);
+
+        getLinkStatus(program);
+
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+
+        return program;
+    }
+
+    GLuint createProgram(filesystem::Path& path, const std::string& vertexShaderFilename, const std::string& fragmentShaderFilename)
+    {
+        filesystem::File vertexShaderFile(path, vertexShaderFilename);
+        filesystem::File fragmentShaderFile(path, fragmentShaderFilename);
+
+        std::string_view vertexShaderSource(reinterpret_cast<const char*>(vertexShaderFile.data()), vertexShaderFile.size());
+        std::string_view fragmentShaderSource(reinterpret_cast<const char*>(fragmentShaderFile.data()), fragmentShaderFile.size());
+
+        return createProgram(vertexShaderSource, fragmentShaderSource);
+    }
+
+    GLuint createProgram(filesystem::Path& path, const std::vector<std::string>& vertexShaderFilenames, const std::vector<std::string>& fragmentShaderFilenames)
+    {
+        struct SourceInfo
+        {
+            size_t offset;
+            u64 size;
+        };
+
+        std::vector<SourceInfo> vertexShaderInfo;
+        std::vector<SourceInfo> fragmentShaderInfo;
+
+        // load shader sources into shared buffer and store where the files were loaded
+
+        Buffer buffer;
+
+        for (const std::string& filename : vertexShaderFilenames)
+        {
+            filesystem::File file(path, filename);
+
+            SourceInfo info { buffer.size(), file.size() };
+            vertexShaderInfo.push_back(info);
+
+            buffer.append(file);
+        }
+
+        for (const std::string& filename : fragmentShaderFilenames)
+        {
+            filesystem::File file(path, filename);
+
+            SourceInfo info { buffer.size(), file.size() };
+            fragmentShaderInfo.push_back(info);
+
+            buffer.append(file);
+        }
+
+        // resolve buffer into string views
+
+        std::vector<std::string_view> vertexShaderSources;
+        std::vector<std::string_view> fragmentShaderSources;
+
+        const char* data = reinterpret_cast<const char*>(buffer.data());
+
+        for (const SourceInfo& info : vertexShaderInfo)
+        {
+            vertexShaderSources.emplace_back(data + info.offset, info.size);
+        }
+
+        for (const SourceInfo& info : fragmentShaderInfo)
+        {
+            fragmentShaderSources.emplace_back(data + info.offset, info.size);
+        }
+
+        return createProgram(vertexShaderSources, fragmentShaderSources);
     }
 
 } // namespace mango::opengl
