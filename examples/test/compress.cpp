32d5ebcb9f8101a24ec886755d49a5d27f1adbee
t0rakka <jukka@liimatta.org>
2025-02-11 17:27:57 +0200

    Compression benchmark.

--- examples/test/compress.cpp
+++ examples/test/compress.cpp
@@ -1,66 +1,79 @@
 /*
     MANGO Multimedia Development Platform
-    Copyright (C) 2012-2024 Twilight Finland 3D Oy Ltd. All rights reserved.
+    Copyright (C) 2012-2025 Twilight Finland 3D Oy Ltd. All rights reserved.
 */
 #include <mango/core/core.hpp>
 
 using namespace mango;
 
-/*
-    NOTE!
-    The compression API does not do any memory management; all
-    input and output buffers have to be allocated by the caller.
-*/
-
-void compression_example(size_t size)
+void test_compression(size_t size, int level)
 {
-    // allocate a buffer and initialize it with junk
-
     Buffer buffer(size);
 
+    // Create the best possible test data (this is fairly impressive achievement)
+    int xd = rand();
     for (size_t i = 0; i < size; ++i)
     {
         buffer[i] = ((i + 2) * 0x123456) & 0xff;
+        buffer[i] += xd;
+        if (!(i % 5)) xd = rand();
     }
 
-    u64 time0 = Time::us();
+    printLine("------------------------------------------------------------");
+    printLine("Method      Ratio        Compress       Decompress    Status");
+    printLine("------------------------------------------------------------");
 
-    // compute maximum compressed size
-    size_t bound = zstd::bound(size);
+    std::vector<Compressor> compressors = getCompressors();
 
-    // allocate compressed buffer
-    Buffer compressed(bound);
+    for (const auto& compressor : compressors)
+    {
+        if (compressor.name.find('.') != std::string::npos)
+        {
+            // filter out compressor where name contains "."; those are just different variations
+            continue;
+        }
 
-    // compress
-    size_t bytes = zstd::compress(compressed, buffer, 6);
+        // compute maximum compressed size
+        size_t bound = compressor.bound(size);
 
-    u64 time1 = Time::us();
+        // allocate compressed buffer
+        Buffer compressed(bound);
 
-    // allocate output buffer
-    Buffer output(size);
+        // allocate output buffer
+        Buffer output(size);
 
-    // decompress
-    // We don't pass the whole compressed buffer but just slice of it
-    // from the beginning which is the size of compressed data.
-    zstd::decompress(output, Memory(compressed, bytes));
+        u64 time0 = Time::us();
 
-    u64 time2 = Time::us();
+        // compress
+        size_t bytes = compressor.compress(compressed, buffer, level);
 
-    // report results
+        u64 time1 = Time::us();
 
-    float rate0 = size / float(time1 - time0);
-    float rate1 = size / float(time2 - time1);
+        // decompress
+        compressor.decompress(output, Memory(compressed, bytes));
 
-    bool correct = std::memcmp(buffer, output, size) == 0;
-    const char* status = correct ? "PASSED" : "FAILED";
+        u64 time2 = Time::us();
 
-    printLine("compressed {} bytes to {} bytes in {} us ({:.1f} MB/s).", size, bytes, time1 - time0, rate0);
-    printLine("decompressed {} bytes to {} bytes in {} us ({:.1f} MB/s).", bytes, size, time2 - time1, rate1);
-    printLine("status: {}", status);
+        bool correct = std::memcmp(buffer, output, size) == 0;
+        const char* status = correct ? "PASSED" : "FAILED";
+
+        float ratio = bytes * 100.0f / size;
+        float rate0 = size / float(time1 - time0);
+        float rate1 = size / float(time2 - time1);
+
+        printLine("{:<9} {:>6.1f}% {:>10.1f} MB/s {:>11.1f} MB/s    {}", compressor.name, ratio, rate0, rate1, status);
+    }
 }
 
 int main(int argc, const char* argv[])
 {
     const size_t size = 1024 * 1024;
-    compression_example(size);
+    int level = 4;
+
+    if (argc == 2)
+    {
+        level = std::atoi(argv[1]);
+    }
+
+    test_compression(size, level);
 }
